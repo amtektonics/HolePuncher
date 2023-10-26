0cabe3206a1126381cd8ebea61b21804e3c3e2e3
Andrew McGuire <amtektonics@gmail.com>
2023-10-26 10:29:04 -0400

    Update holepunch_node.gd
    Updated to work on Godot 4.1.1

--- addons/Holepunch/holepunch_node.gd
+++ addons/Holepunch/holepunch_node.gd
@@ -1,5 +1,7 @@
 extends Node
 
+class_name HolePunch
+
 #Signal is emitted when holepunch is complete. Connect this signal to your network manager
 #Once your network manager received the signal they can host or join a game on the host port
 signal hole_punched(my_port, hosts_port, hosts_address)
@@ -12,13 +14,13 @@ var server_udp = PacketPeerUDP.new()
 var peer_udp = PacketPeerUDP.new()
 
 #Set the rendevouz address to the IP address of your third party server
-export(String) var rendevouz_address = "" 
+@export var rendevouz_address = "" 
 #Set the rendevouz port to the port of your third party server
-export(int) var rendevouz_port = 4000
+@export var rendevouz_port = 4000
 #This is the range of ports you will search if you hear no response from the first port tried
-export(int) var port_cascade_range = 10
+@export var port_cascade_range = 10
 #The amount of messages of the same type you will send before cascading or giving up
-export(int) var response_window = 5
+@export var response_window = 5
 
 
 var found_server = false
@@ -88,7 +90,7 @@ func _process(delta):
 		if not recieved_peer_info:
 			if packet_string.begins_with(SERVER_INFO):
 				server_udp.close()
-				packet_string = packet_string.right(6)
+				packet_string = packet_string.right(-6)
 				if packet_string.length() > 2:
 					var m = packet_string.split(":")
 					peer[m[0]] = {"port":m[2], "address":m[1]}
@@ -100,7 +102,7 @@ func _handle_greet_message(peer_name, peer_port, my_port):
 	if own_port != my_port:
 		own_port = my_port
 		peer_udp.close()
-		peer_udp.listen(own_port, "*")
+		peer_udp.bind(own_port, "*")
 	recieved_peer_greet = true
 
 
@@ -113,7 +115,7 @@ func _handle_confirm_message(peer_name, peer_port, my_port, is_host):
 		host_address = peer[peer_name].address
 		host_port = peer[peer_name].port
 	peer_udp.close()
-	peer_udp.listen(own_port, "*")
+	peer_udp.bind(own_port, "*")
 	recieved_peer_confirm = true
 
 
@@ -128,8 +130,8 @@ func _handle_go_message(peer_name):
 func _cascade_peer(add, peer_port):
 	for i in range(peer_port - port_cascade_range, peer_port + port_cascade_range):
 		peer_udp.set_dest_address(add, i)
-		var buffer = PoolByteArray()
-		buffer.append_array(("greet:"+client_name+":"+str(own_port)+":"+str(i)).to_utf8())
+		var buffer = PackedByteArray()
+		buffer.append_array(("greet:"+client_name+":"+str(own_port)+":"+str(i)).to_utf8_buffer())
 		peer_udp.put_packet(buffer)
 		ports_tried += 1
 
@@ -139,8 +141,8 @@ func _ping_peer():
 	if not recieved_peer_confirm and greets_sent < response_window:
 		for p in peer.keys():
 			peer_udp.set_dest_address(peer[p].address, int(peer[p].port))
-			var buffer = PoolByteArray()
-			buffer.append_array(("greet:"+client_name+":"+str(own_port)+":"+peer[p].port).to_utf8())
+			var buffer = PackedByteArray()
+			buffer.append_array(("greet:"+client_name+":"+str(own_port)+":"+peer[p].port).to_utf8_buffer())
 			peer_udp.put_packet(buffer)
 			greets_sent+=1
 			if greets_sent == response_window:
@@ -155,15 +157,15 @@ func _ping_peer():
 	if recieved_peer_greet and not recieved_peer_go:
 		for p in peer.keys():
 			peer_udp.set_dest_address(peer[p].address, int(peer[p].port))
-			var buffer = PoolByteArray()
-			buffer.append_array(("confirm:"+str(own_port)+":"+client_name+":"+str(is_host)+":"+peer[p].port).to_utf8())
+			var buffer = PackedByteArray()
+			buffer.append_array(("confirm:"+str(own_port)+":"+client_name+":"+str(is_host)+":"+peer[p].port).to_utf8_buffer())
 			peer_udp.put_packet(buffer)
 
 	if  recieved_peer_confirm:
 		for p in peer.keys():
 			peer_udp.set_dest_address(peer[p].address, int(peer[p].port))
-			var buffer = PoolByteArray()
-			buffer.append_array(("go:"+client_name).to_utf8())
+			var buffer = PackedByteArray()
+			buffer.append_array(("go:"+client_name).to_utf8_buffer())
 			peer_udp.put_packet(buffer)
 		gos_sent += 1
 
@@ -173,12 +175,12 @@ func _ping_peer():
 			set_process(false)
 
 
-func start_peer_contact():	
-	server_udp.put_packet("goodbye".to_utf8())
+func start_peer_contact():
+	server_udp.put_packet("goodbye".to_utf8_buffer())
 	server_udp.close()
-	if peer_udp.is_listening():
+	if peer_udp.is_bound():
 		peer_udp.close()
-	var err = peer_udp.listen(own_port, "*")
+	var err = peer_udp.bind(own_port, "*")
 	if err != OK:
 		print("Error listening on port: " + str(own_port) +" Error: " + str(err))
 	p_timer.start()
@@ -186,26 +188,26 @@ func start_peer_contact():
 
 #this function can be called to the server if you want to end the holepunch before the server closes the session
 func finalize_peers(id):
-	var buffer = PoolByteArray()
-	buffer.append_array((EXCHANGE_PEERS+str(id)).to_utf8())
+	var buffer = PackedByteArray()
+	buffer.append_array((EXCHANGE_PEERS+str(id)).to_utf8_buffer())
 	server_udp.set_dest_address(rendevouz_address, rendevouz_port)
 	server_udp.put_packet(buffer)
 
 
 # remove a client from the server
 func checkout():
-	var buffer = PoolByteArray()
-	buffer.append_array((CHECKOUT_CLIENT+client_name).to_utf8())
+	var buffer = PackedByteArray()
+	buffer.append_array((CHECKOUT_CLIENT+client_name).to_utf8_buffer())
 	server_udp.set_dest_address(rendevouz_address, rendevouz_port)
 	server_udp.put_packet(buffer)
 
 
 #Call this function when you want to start the holepunch process
 func start_traversal(id, is_player_host, player_name):
-	if server_udp.is_listening():
+	if server_udp.is_bound():
 		server_udp.close()
 
-	var err = server_udp.listen(rendevouz_port, "*")
+	var err = server_udp.bind(rendevouz_port, "*")
 	if err != OK:
 		print("Error listening on port: " + str(rendevouz_port) + " to server: " + rendevouz_address)
 
@@ -224,8 +226,8 @@ func start_traversal(id, is_player_host, player_name):
 	session_id = id
 	
 	if (is_host):
-		var buffer = PoolByteArray()
-		buffer.append_array((REGISTER_SESSION+session_id+":"+str(MAX_PLAYER_COUNT)).to_utf8())
+		var buffer = PackedByteArray()
+		buffer.append_array((REGISTER_SESSION+session_id+":"+str(MAX_PLAYER_COUNT)).to_utf8_buffer())
 		server_udp.close()
 		server_udp.set_dest_address(rendevouz_address, rendevouz_port)
 		server_udp.put_packet(buffer)
@@ -235,9 +237,9 @@ func start_traversal(id, is_player_host, player_name):
 
 #Register a client with the server
 func _send_client_to_server():
-	yield(get_tree().create_timer(2.0), "timeout")
-	var buffer = PoolByteArray()
-	buffer.append_array((REGISTER_CLIENT+client_name+":"+session_id).to_utf8())
+	await get_tree().create_timer(2.0).timeout
+	var buffer = PackedByteArray()
+	buffer.append_array((REGISTER_CLIENT+client_name+":"+session_id).to_utf8_buffer())
 	server_udp.close()
 	server_udp.set_dest_address(rendevouz_address, rendevouz_port)
 	server_udp.put_packet(buffer)
@@ -250,5 +252,5 @@ func _exit_tree():
 func _ready():
 	p_timer = Timer.new()
 	get_node("/root/").call_deferred("add_child", p_timer)
-	p_timer.connect("timeout", self, "_ping_peer")
+	p_timer.timeout.connect(_ping_peer)
 	p_timer.wait_time = 0.1
